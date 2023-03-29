9d194947f72e2f3376ff53891a856b71b3b02df9
RotemEzra1 <Rotem19961@gmail.com>
2023-03-29 20:15:15 +0300

    logo add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me3.png'
+import LOGO from '../../assets/logo3.png'
 import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
     <header>
@@ -11,6 +13,7 @@ const Header = () => {
         <h1>Rotem Ezra</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <h5 className="text-light">Bachelor's Degree in software Engineering</h5>
+        <img src={LOGO} className='header__logo' />
         <CTA />
         <HeaderSocials />
         <div className="me">

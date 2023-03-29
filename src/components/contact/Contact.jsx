604dcc446731868fac006086ec546751a567d6b0
RotemEzra1 <Rotem19961@gmail.com>
2023-03-29 19:44:27 +0300

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -39,14 +39,14 @@ const Contact = () => {
           <article className='contact__option'>
             <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
-            <h5>Rotem Ezra facebook</h5>
+            <h5>Contact me on facebook</h5>
             <a href="https://m.me/rotem.ezra.7" target={'_blank'}>Send a message</a>
           </article>
 
           <article className='contact__option'>
             <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
-            <h5>--</h5>
+            <h5>Let's Tallk Kapara al lechem</h5>
             <a href="https://api.whatsapp.com/send?phone=972506617695" target={'_blank'}>Send a message</a>
           </article>
         </div>
